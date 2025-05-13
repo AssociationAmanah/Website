4705374404f9c5f4a208bdaeb018378ab5cee66a
AssociationAmanah <eleonoresalgado@gmail.com>
2025-05-13 15:13:39 +0200

    Déploiement des fichiers de production

--- project-bolt-sb1-eqcsmbxc/project/vite.config.ts
+++ project-bolt-sb1-eqcsmbxc/project/vite.config.ts
@@ -7,4 +7,10 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  build: {
+    outDir: 'dist', // Assure que la sortie se fait dans le dossier dist
+    rollupOptions: {
+      input: 'index.html', // Utilise index.html à la racine comme point d'entrée
+    },
+  },
 });
